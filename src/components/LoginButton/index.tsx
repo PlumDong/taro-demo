88bd5da391ab1e8f96fd3a8f3818a2e2b2070177
马振东 <mazd@unuo.com>
2023-03-27 17:52:36 +0800

    2023-03-27

---
+++ src/components/LoginButton/index.tsx
@@ -0,0 +1,9 @@
+import { AtButton } from "taro-ui";
+
+export default function LoginButton(props) {
+  return (
+    <AtButton type="primary" onClick={props.handleClick}>
+      普通登录
+    </AtButton>
+  );
+}
